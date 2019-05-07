442428b8e4f79a339822f468b379ca8131997906
bassrullah <bassrullah@gmail.com>
2019-05-07 14:43:04 +0700

    [Bass] - install package and finished slicing

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
+import { Card, CardBody, Row, Col, Button} from 'reactstrap'
 
 export default class PostList extends Component {
   render() {
     return (
       <div>
-        
-      </div>
+						<Card  className="card-border">
+							<CardBody className="px-0 py-0">
+								<Row>
+									<Col sm={1} className="text-center">
+										<Button className="btn-secondary2" >
+											<i className="fa fa-2x fa-arrow-up" />
+										</Button>
+										<p className="upvote-counter">Counter</p>
+									</Col>
+									<Col sm={1} className="text-center">
+										<Button className="btn-secondary2">
+											<i className="fa fa-2x fa-arrow-down"  />
+										</Button>
+										<p className="downvote-counter ">Counter</p>
+									</Col>
+									<Col sm={10} >
+										<h5 className="ml-3 mt-2 text-wrap">Title</h5>
+									</Col>
+								</Row>
+							</CardBody>
+							<hr/>
+						</Card>
+				
+		</div>
     )
   }
 }
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import AddPost from '../components/AddPost'
 import PostList from '../components/PostList'
+import '../assets/main.scss'
 
 export default class LandingPage extends Component {
   render() {
